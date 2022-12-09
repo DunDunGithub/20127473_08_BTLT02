b9c2a33f6caa2280e59c7e08fbc6ab0703e8ff47
_april.inme <dung8ahxh@gmail.com>
2022-12-09 13:44:53 +0700

    Test DB

---
+++ app/src/main/java/com/example/testdatabase/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.testdatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    MyDatabase db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Create database
+        db = new MyDatabase(this, "ProManager.sqlite", null, 1);
+
+        //Create table
+        String createUserInfo = "CREATE TABLE IF NOT EXISTS UserInfo(username VARCHAR(100) PRIMARY KEY," +
+                "pass VARCHAR(100)," +
+                "fullname NVARCHAR(100)," +
+                "overview NVARCHAR(1000)," +
+                "totalTasks smallint," +
+                "totalHours smallint," +
+                "currentTasks smallint," +
+                "currentFinished smallint)";
+        db.queryData(createUserInfo);
+
+        //Add data
+        String addStatement1 = "INSERT INTO UserInfo VALUES ('username1', '123456', 'US1', 'ABC', 10, 30, 3, 7)";
+        String addStatement2 = "INSERT INTO UserInfo VALUES ('username2', '123456', 'US2', 'DEF', 5, 24, 1, 4)";
+        db.queryData(addStatement1);
+        db.queryData(addStatement2);
+
+        //Get data
+        Cursor dataUserInfo = db.getData("SELECT * FROM UserInfo");
+        while(dataUserInfo.moveToNext()){
+            String name = dataUserInfo.getString(0);
+            Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/testdatabase/MyDatabase.java
@@ -0,0 +1,38 @@
+package com.example.testdatabase;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MyDatabase extends SQLiteOpenHelper {
+
+    public MyDatabase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    //Truy vấn không trả kết quả
+    public void queryData(String sql){
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(sql);
+    }
+
+    //Truy vấn trả kết quả
+    public Cursor getData(String sql){
+        SQLiteDatabase db = getReadableDatabase();
+        return db.rawQuery(sql, null);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
